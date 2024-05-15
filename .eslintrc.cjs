4db87e72b7f76978fca3f0ceae5967ccdb6f02df
Seth Nickell <snickell@gmail.com>
2024-05-15 02:57:48 -1000

    `@typescript-eslint/no-explicit-any` makes prototyping hard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    // This makes prototyping a lot harder:
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 }
