2ac93a585f1c4b8d7f34109d7abc6ffc5ebab767
Seth Nickell <snickell@gmail.com>
2024-05-15 01:09:26 -1000

    Use new-style functions for Lab.tsx

--- src/Lab.tsx
+++ src/Lab.tsx
@@ -1,30 +1,24 @@
 import {CodeOrg, BlockWorkspace, Header, Output} from '../libprotolab/layout/CodeOrg'
 import {blocklyOptions} from './blocks'
 import {save, load, run} from 'libprotolab/blockly/workspace'
+
 export const LAB_NAME = "Protolab"
 
-function Toolbar() {
-  const toolbarStyle = {
+const Toolbar = () =>
+  <div style={{
     display: 'flex',
     justifyContent: 'flex-end',
     gap: 4,
-  }
-  return (
-    <div style={toolbarStyle}>
-      <button onClick={() => run()}>Run</button>
-      <button>Reset</button>
-      <button onClick={() => save()}>Save</button>
-      <button onClick={() => load()}>Load</button>
-    </div>
-  )
-}
+  }}>
+    <button onClick={() => run()}>Run</button>
+    <button>Reset</button>
+    <button onClick={() => save()}>Save</button>
+    <button onClick={() => load()}>Load</button>
+  </div>
 
-export function Lab() {
-  return (
-    <CodeOrg
-      header={<Header toolbar=<Toolbar/> />}
-      workspace={<BlockWorkspace blocklyOptions={blocklyOptions} />}
-      output={<Output />}
-    />
-  )
-}
+export const Lab = () =>
+  <CodeOrg
+    header={<Header toolbar=<Toolbar/> />}
+    workspace={<BlockWorkspace blocklyOptions={blocklyOptions} />}
+    output={<Output />}
+  />
