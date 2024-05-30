ddc080113d3d0eb32badee3cf6faf8e16138668e
Seth Nickell <snickell@gmail.com>
2024-05-29 21:21:01 -1000

    Simplify the types, favor the JS definition

--- libprotolab/blockly/define-block.ts
+++ libprotolab/blockly/define-block.ts
@@ -2,37 +2,31 @@ import Blockly, { Block } from "blockly";
 import { JavascriptGenerator, javascriptGenerator, Order } from "blockly/javascript";
 export { defineBlocksInPoolStyle } from "./define-blocks-in-pool-style";
 
-export type JSCodeString = string;
-
-// For generating JS values, see: https://developers.google.com/blockly/guides/create-custom-blocks/code-generation/values/basic-implementation
-export type JSValue = [JSCodeString, Order];
-
-// For generating JS statements, see: https://developers.google.com/blockly/guides/create-custom-blocks/code-generation/statements/basic-implementation
-export type JSStatement = JSCodeString;
-
-// Generates the JS code for a given custom block, the JS code can either
-// define a JS statement, or a JS value.
-export type BlocklyJSGenerator = (block: Block, generator: JavascriptGenerator) => JSValue | JSStatement;
-
-export type BlockJSON = object;
-export type InitializerFunc = (block: Block) => void;
-
-export function defineBlock(
-  type: string,
-  jsGenerator: BlocklyJSGenerator,
-  definition: BlockJSON | InitializerFunc
-) {
-  if (definition instanceof Function) {
+export function defineBlock({
+  type,
+  jsGenerator,
+  blocklyJSON,
+  init: init,
+}: {
+  type: string;
+  jsGenerator: (
+    block: Block,
+    generator: JavascriptGenerator
+  ) => string | [string, Order];
+  blocklyJSON?: object;
+  init?: (block: Block) => void;
+}) {
+  if (init) {
     Blockly.Blocks[type] = {
       init: function () {
-        definition(this);
+        init.bind(this)(this);
       },
     };
   } else {
-    const blockJSON = { ...definition, type: type };
+    blocklyJSON = { ...blocklyJSON, type: type };
     Blockly.Blocks[type] = {
       init: function () {
-        this.jsonInit(blockJSON);
+        this.jsonInit(blocklyJSON);
       },
     };
   }
--- src/blocks/blocks.ts
+++ src/blocks/blocks.ts
@@ -4,16 +4,59 @@ import "./pool-style-blocks";
 
 // This is an example of a `statement` block with one input:
 // https://developers.google.com/blockly/guides/create-custom-blocks/code-generation/statements/basic-implementation
-export const alertBlock = defineBlock(
-  "alert",
-  (block, generator) => {
+export const alertBlock = defineBlock({
+  type: "alert",
+  jsGenerator: (block, generator) => {
+    const value = generator.valueToCode(block, "VALUE", 0);
+
+    // Statement blocks are just a string of the JS you want to generate:
+    return `alert(${value})`;
+  },
+  init: function (block) {
+    block.appendValueInput("VALUE")
+      .setCheck("String")
+      .appendField("alert");
+
+    block.setPreviousStatement(true);
+    block.setNextStatement(true);
+
+    block.setColour(160);
+    block.setTooltip("Display an alert box with the provided text.");
+  }
+});
+
+// This is an example of a `value` block with one input:
+// https://developers.google.com/blockly/guides/create-custom-blocks/code-generation/values/basic-implementation
+export const toUpperCaseBlock = defineBlock({
+  type: "toUpperCase",
+  jsGenerator: (block, generator) => {
+    const value = generator.valueToCode(block, "VALUE", 0);
+
+    // Value blocks are the string of the JS you want to generate, AND an operator precedence:
+    // https://developers.google.com/blockly/guides/create-custom-blocks/code-generation/values/operator-precedence
+    return [`(${value}).toUpperCase()`, Order.NONE];
+  },
+  init: function (block) {
+    block.appendValueInput("VALUE")
+      .setCheck("String")
+      .appendField("toUpperCase");
+
+    block.setColour(160);
+    block.setTooltip("Display an alert box with the provided text.");
+  }
+});
+
+// You can also define blocks using blocklyJSON instead of an init function:
+export const alertBlock2 = defineBlock({
+  type: "alert2",
+  jsGenerator: (block, generator) => {
     const value = generator.valueToCode(block, "VALUE", 0);
 
     // Statement blocks are just a string of the JS you want to generate
     return `alert(${value})`;
   },
-  {
-    message0: "alert %1",
+  blocklyJSON: {
+    message0: "alert2 %1",
     args0: [
       {
         type: "input_value",
@@ -26,56 +69,7 @@ export const alertBlock = defineBlock(
     colour: 160,
     tooltip: "Display an alert box with the provided text.",
   }
-);
-
-// This is an example of a `value` block with one input:
-// https://developers.google.com/blockly/guides/create-custom-blocks/code-generation/values/basic-implementation
-export const toUpperCaseBlock = defineBlock(
-  "toUpperCase",
-  (block, generator) => {
-    const value = generator.valueToCode(block, "VALUE", 0);
-
-    // Value blocks require both the generated JS, and specifying an operator precedence:
-    // https://developers.google.com/blockly/guides/create-custom-blocks/code-generation/values/operator-precedence
-    return [`(${value}).toUpperCase()`, Order.NONE];
-  },
-  {
-    message0: "toUpperCase %1",
-    args0: [
-      {
-        type: "input_value",
-        name: "VALUE",
-        check: "String",
-      },
-    ],
-    output: "String",
-    colour: 160,
-    tooltip: "Converts the provided text to uppercase.",
-  }
-);
-
-// This is an example of a `statement` block with one input:
-// https://developers.google.com/blockly/guides/create-custom-blocks/code-generation/statements/basic-implementation
-export const alertBlock2 = defineBlock(
-  "alert",
-  (block, generator) => {
-    const value = generator.valueToCode(block, "VALUE", 0);
-
-    // Statement blocks are just a string of the JS you want to generate
-    return `alert(${value})`;
-  },
-  function (block) {
-    block.appendValueInput("VALUE")
-      .setCheck("String")
-      .appendField("alert2");
-
-    block.setPreviousStatement(true);
-    block.setNextStatement(true);
-
-    block.setColour(160);
-    block.setTooltip("Display an alert box with the provided text.");
-  }
-);
+});
 
 export const here = defineBlock(
   "here",
