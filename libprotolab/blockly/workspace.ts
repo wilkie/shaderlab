1e295adee6f376ac868e002f3bd0a08907ef6a32
Seth Nickell <snickell@gmail.com>
2024-05-29 22:00:39 -1000

    Basic save

--- libprotolab/blockly/workspace.ts
+++ libprotolab/blockly/workspace.ts
@@ -1,10 +1,14 @@
 import Blockly from "blockly";
 export { run } from './run'
 
+const DEFAULT_SAVE_NAME = "default";
+
 let workspace: Blockly.Workspace | null = null;
+let saveName = new URLSearchParams(window.location.search).get("saveName");
 
 export function setWorkspace(_workspace: Blockly.Workspace) {
   workspace = _workspace;
+  load();
 }
 
 export function getWorkspace() {
@@ -12,12 +16,21 @@ export function getWorkspace() {
   return workspace;
 }
 
-export function save(saveName: string = "workspace") {
+export function save() {
+  saveName =
+    prompt("Save as:", saveName || DEFAULT_SAVE_NAME) || DEFAULT_SAVE_NAME;
   const state = Blockly.serialization.workspaces.save(getWorkspace());
   localStorage.setItem(saveName, JSON.stringify(state));
+
+  // Update the URL
+  const url = new URL(window.location.href);
+  url.searchParams.set("saveName", saveName);
+  window.history.pushState({}, "", url.toString());
 }
 
-export function load(saveName: string = "workspace") {
+export function load() {
+  if (!saveName) return;
+
   const state = localStorage.getItem(saveName);
   if (state) {
     Blockly.serialization.workspaces.load(JSON.parse(state), getWorkspace());
